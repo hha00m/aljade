2c398df01b9000293c8d758c65d5993b2c19ccdb
hha00m <hayder_ok@yahoo.com>
2020-09-20 00:36:32 +0300

    update

--- src/pages/models/components/BasketsModel.jsx
+++ src/pages/models/components/BasketsModel.jsx
@@ -7,7 +7,7 @@ import { activeModelMethod, } from '../store/actions';
 import { closeModelMethod, } from '../store/actions';
 import {
     cencelSendBasketToDB, fetchingBasketsMethod, deleteBasketsMethod,
-    sendBasketToDB, selectedBasketMethod, addItemToBasket, all_selectedBasketsMethod, fetchingBasketByIDMethod,activeBasketsModelMethod
+    sendBasketToDB, selectedBasketMethod, addItemToBasket, all_selectedBasketsMethod, fetchingBasketByIDMethod, activeBasketsModelMethod
 } from './../store/actions/index';
 import './_style.css';
 const RadioItem = Radio.RadioItem;
@@ -38,8 +38,8 @@ class BasketsModel extends React.Component {
                         && this.props.ActiveModel.action}
                     onClose={
                         () => {
-                            this.props.activeBasketsModelMethod(false);
-                            this.props.closeModelMethod(this.props.modelList[1]);
+                            this.props.basketListShow.show ? this.props.activeBasketsModelMethod(false) :
+                                this.props.closeModelMethod(this.props.modelList[1]);
                         }
                     }
                     animationType="slide-up"
@@ -176,7 +176,7 @@ function matchDispatchToProps(dispatch) {
             addItemToBasket: addItemToBasket,
             all_selectedBasketsMethod: all_selectedBasketsMethod,
             fetchingBasketByIDMethod: fetchingBasketByIDMethod,
-            activeBasketsModelMethod:activeBasketsModelMethod,
+            activeBasketsModelMethod: activeBasketsModelMethod,
         }, dispatch);
 }
 export default connect(mapStateToProps, matchDispatchToProps)(BasketsModel);
